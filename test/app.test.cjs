251620344764b84b6f5352dd2e0fee804737e650
Amina Ghannem <amynaghannem@gmail.com>
2025-10-19 18:03:12 +0100

    add workflow

---
+++ test/app.test.cjs
@@ -0,0 +1,11 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const request = require('supertest');
+
+const app = require('../dist/app.js').default;
+
+test('GET / returns Hello!', async () => {
+  const res = await request(app).get('/');
+  assert.equal(res.statusCode, 200);
+  assert.equal(res.text, 'Hello DAST!');
+});
