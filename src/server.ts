820163f549473ec4936361a4caaba262aaacc8f8
Amina Ghannem <amynaghannem@gmail.com>
2025-10-19 17:44:28 +0100

    first commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello DAST!');
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${port}`);
+});
