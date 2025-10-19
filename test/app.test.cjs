23bcbe5eea4cd3f8fe59e24ac70c76530ef413d7
Amina Ghannem <amynaghannem@gmail.com>
2025-10-19 18:05:18 +0100

    fix test

--- test/app.test.cjs
+++ test/app.test.cjs
@@ -7,5 +7,5 @@ const app = require('../dist/app.js').default;
 test('GET / returns Hello!', async () => {
   const res = await request(app).get('/');
   assert.equal(res.statusCode, 200);
-  assert.equal(res.text, 'Hello DAST!');
+  assert.equal(res.text, 'Hello!');
 });
