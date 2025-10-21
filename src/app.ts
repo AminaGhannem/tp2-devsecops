7ef3f6b9c1b816e1c3749393eda3d9d3032c8c50
Amina Ghannem <amynaghannem@gmail.com>
2025-10-21 19:19:21 +0100

    test build

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = req.query.name || 'World';
-  // Reflected XSS vulnerability (runtime issue, not static obvious)
-  res.send(`<h1>Hello ${name}!</h1>`);
+  res.send('Hello!');
 });
 
 export default app;
